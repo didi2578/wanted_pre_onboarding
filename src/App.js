3731aa8cff8ddadbcc5313f65cd9e5c577439319
username <storyno7@gmail.com>
2022-02-08 00:38:07 +0900

    🚧Done with tap function.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function App() {
-  return <div className="App"></div>
+const content = [
+  { tab: 'Tab 1', content: 'ONE' },
+  { tab: 'Tab 2', content: 'TWO' },
+  { tab: 'Tab 3', content: 'THREE' },
+]
+const useTab = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab)
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  }
+}
+
+const App = () => {
+  const { currentItem, changeItem } = useTab(0, content)
+  return (
+    <>
+      {content.map((tabs, index) => (
+        <button onClick={() => changeItem(index)}>{tabs.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </>
+  )
 }
 
 export default App
