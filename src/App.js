fffb9d6a0cc46e085069cc07e4e78b19c0ba0a09
username <storyno7@gmail.com>
2022-02-08 00:50:27 +0900

    🚧Done with the summary of tap functions.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { useState } from 'react'
 
-const content = [
-  { tab: 'Tab 1', content: 'ONE' },
-  { tab: 'Tab 2', content: 'TWO' },
-  { tab: 'Tab 3', content: 'THREE' },
+const tabs = [
+  { num: 'Tab 1', content: 'ONE' },
+  { num: 'Tab 2', content: 'TWO' },
+  { num: 'Tab 3', content: 'THREE' },
 ]
-const useTab = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab)
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex,
-  }
-}
 
 const App = () => {
-  const { currentItem, changeItem } = useTab(0, content)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
   return (
     <>
-      {content.map((tabs, index) => (
-        <button onClick={() => changeItem(index)}>{tabs.tab}</button>
+      {tabs.map((tab, index) => (
+        <button onClick={() => setCurrentIndex(index)}>{tab.num}</button>
       ))}
-      <div>{currentItem.content}</div>
+      <p>Tab menu {tabs[currentIndex].content}</p>
     </>
   )
 }
